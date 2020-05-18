5e46a6dfda739c230c53c35bd443deeb2142b2b6
fdrouhin <Frederic.Drouhin@gmail.com>
2020-05-18 15:29:35 +0200

    Update BookInfo.java

--- app/src/main/java/com/example/ayoub/booky/BookInfo.java
+++ app/src/main/java/com/example/ayoub/booky/BookInfo.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
-public class BookInfo extends AppCompatActivity {
+public class BookInfoActivity extends AppCompatActivity {
 
 
     private static final String  TAG = BookInfo.class.getSimpleName() ;
